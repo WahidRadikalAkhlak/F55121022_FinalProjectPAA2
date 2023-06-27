d7a86a4029a05e1e408b2238364ad43956f14098
Wahid <ikalstm51@gmail.com>
2023-06-27 21:53:17 +0800

    Coomit

--- TSP & Djikstra.py
+++ TSP & Djikstra.py
@@ -1,161 +1,148 @@
-import sys
+import networkx as nx
 import time
-import itertools
 
+def tsp(graph, start):
+    def node_to_int(node):
+        return ord(node) - ord('A')
 
-class Graph:
-    def __init__(self):
-        self.vertices = set()
-        self.edges = {}
+    def int_to_node(num):
+        return chr(num + ord('A'))
 
-    def add_edge(self, start, end, weight):
-        self.vertices.add(start)
-        self.vertices.add(end)
-        if start not in self.edges:
-            self.edges[start] = {}
-        self.edges[start][end] = weight
-
-
-def tsp(graph, start, end):
+    n = graph.number_of_nodes()
+    visited = [False] * n
+    path = []
+    path.append(start)
+    visited[node_to_int(start)] = True
+    total_weight = 0
     start_time = time.time()
-    all_paths = []
-    shortest_path = None
-    shortest_distance = sys.maxsize
 
-    all_permutations = itertools.permutations(graph.vertices)
+    def tsp_util(curr):
+        nonlocal total_weight
+        min_weight = float('inf')
+        next_node = None
+        for neighbor in graph.neighbors(int_to_node(curr)):
+            weight = graph[int_to_node(curr)][neighbor]['weight']
+            if not visited[node_to_int(neighbor)] and weight < min_weight:
+                min_weight = weight
+                next_node = neighbor
+        if next_node is not None:
+            path.append(next_node)
+            visited[node_to_int(next_node)] = True
+            total_weight += min_weight
+            tsp_util(node_to_int(next_node))
 
-    for permutation in all_permutations:
-        if permutation[0] != start or permutation[-1] != end:
-            continue
-
-        distance = 0
-        path = []
-        for i in range(len(permutation) - 1):
-            current_vertex = permutation[i]
-            next_vertex = permutation[i + 1]
-
-            if next_vertex in graph.edges[current_vertex]:
-                distance += graph.edges[current_vertex][next_vertex]
-                path.append(current_vertex)
-
-        path.append(next_vertex)
-
-        if distance < shortest_distance:
-            shortest_distance = distance
-            shortest_path = path
-
-        all_paths.append((path, distance))
-
+    tsp_util(node_to_int(start))
     end_time = time.time()
-    execution_time = end_time - start_time
-
-    return shortest_path, shortest_distance, all_paths, execution_time
-
+    return path, total_weight, end_time - start_time
 
 def dijkstra(graph, start, end):
+    dist = {}
+    prev = {}
+    unvisited = set(graph.nodes())
     start_time = time.time()
-    distances = {}
-    previous_vertices = {}
-    unvisited_vertices = graph.vertices.copy()
 
-    for vertex in graph.vertices:
-        distances[vertex] = sys.maxsize
-        previous_vertices[vertex] = None
-    distances[start] = 0
+    def node_to_int(node):
+        return ord(node) - ord('A')
 
-    while unvisited_vertices:
-        current_vertex = min(unvisited_vertices, key=lambda vertex: distances[vertex])
+    def int_to_node(num):
+        return chr(num + ord('A'))
 
-        if current_vertex == end:
-            break
+    for node in graph.nodes():
+        dist[node] = float('inf')
+    dist[start] = 0
 
-        unvisited_vertices.remove(current_vertex)
+    while unvisited:
+        min_dist = float('inf')
+        curr = None
+        for node in unvisited:
+            if dist[node] < min_dist:
+                min_dist = dist[node]
+                curr = node
+        unvisited.remove(curr)
 
-        for neighbor in graph.edges[current_vertex]:
-            alternative_distance = distances[current_vertex] + graph.edges[current_vertex][neighbor]
-
-            if alternative_distance < distances[neighbor]:
-                distances[neighbor] = alternative_distance
-                previous_vertices[neighbor] = current_vertex
-
-    shortest_path = []
-    current_vertex = end
-
-    while current_vertex is not None:
-        shortest_path.insert(0, current_vertex)
-        current_vertex = previous_vertices[current_vertex]
+        for neighbor in graph.neighbors(curr):
+            weight = graph[curr][neighbor]['weight']
+            alt = dist[curr] + weight
+            if alt < dist[neighbor]:
+                dist[neighbor] = alt
+                prev[neighbor] = curr
 
     end_time = time.time()
-    execution_time = end_time - start_time
+    path = []
+    curr = end
+    while curr != start:
+        path.append(curr)
+        curr = prev[curr]
+    path.append(start)
+    path.reverse()
+    return path, dist[end], end_time - start_time
 
-    return shortest_path, distances[end], distances, execution_time
+def print_iterations(path):
+    print("Iterasi:")
+    for i, node in enumerate(path):
+        print(f"Iterasi {i+1}: {node}")
+    print()
 
+def print_computation_time(time):
+    print(f"Waktu Komputasi: {time:.6f} detik")
 
-def print_path(path, distance):
-    if path:
-        print("Shortest Path:", " -> ".join(path))
-        print("Total jarak:", distance)
-    else:
-        print("Path tidak ditemukan.")
+def print_shortest_path(path, weight):
+    print("Jalur Terpendek TSP:")
+    print(" -> ".join(path))
+    print(f"Total Bobot: {weight}\n")
 
+def main():
+    graph = nx.Graph()
+    graph.add_edge('A', 'B', weight=12)
+    graph.add_edge('A', 'C', weight=10)
+    graph.add_edge('A', 'G', weight=12)
+    graph.add_edge('B', 'A', weight=12)
+    graph.add_edge('B', 'C', weight=8)
+    graph.add_edge('B', 'D', weight=12)
+    graph.add_edge('C', 'A', weight=10)
+    graph.add_edge('C', 'B', weight=8)
+    graph.add_edge('C', 'D', weight=11)
+    graph.add_edge('C', 'E', weight=3)
+    graph.add_edge('C', 'G', weight=9)
+    graph.add_edge('D', 'C', weight=11)
+    graph.add_edge('D', 'E', weight=11)
+    graph.add_edge('D', 'F', weight=10)
+    graph.add_edge('E', 'C', weight=3)
+    graph.add_edge('E', 'D', weight=11)
+    graph.add_edge('E', 'F', weight=6)
+    graph.add_edge('E', 'G', weight=7)
+    graph.add_edge('F', 'D', weight=10)
+    graph.add_edge('F', 'E', weight=6)
+    graph.add_edge('F', 'G', weight=9)
+    graph.add_edge('G', 'A', weight=12)
+    graph.add_edge('G', 'C', weight=9)
+    graph.add_edge('G', 'E', weight=7)
+    graph.add_edge('G', 'F', weight=9)
 
-def print_all_paths(all_paths, start, end):
-    filtered_paths = [(path, distance) for path, distance in all_paths if path[0] == start and path[-1] == end]
+    while True:
+        print("Pilihan Algoritma:")
+        print("1. TSP (Traveling Salesman Problem)")
+        print("2. Dijkstra")
+        choice = int(input("Masukkan pilihan (1/2): "))
+        if choice == 1:
+            start_node = input("Pilih node Awal: ")
+            tsp_path, tsp_weight, tsp_time = tsp(graph, start_node)
+            print_iterations(tsp_path)
+            print_computation_time(tsp_time)
+            print_shortest_path(tsp_path, tsp_weight)
+        elif choice == 2:
+            start_node = input("Pilih node Awal: ")
+            end_node = input("Pilih node Akhir(Node yang dituju): ")
+            dijkstra_path, dijkstra_weight, dijkstra_time = dijkstra(graph, start_node, end_node)
+            print_iterations(dijkstra_path)
+            print_computation_time(dijkstra_time)
+            print_shortest_path(dijkstra_path, dijkstra_weight)
+        else:
+            print("Pilihan tidak valid.")
 
-    for path, distance in filtered_paths:
-        print("Path:", " -> ".join(path))
-        print("Jarak:", distance)
-        print()
+        repeat = input("Lakukan pengurutan lagi? (y/n): ")
+        if repeat.lower() != 'y':
+            break
 
-
-graph = Graph()
-graph.add_edge('A', 'B', 12)
-graph.add_edge('A', 'C', 10)
-graph.add_edge('A', 'G', 12)
-graph.add_edge('B', 'C', 8)
-graph.add_edge('B', 'D', 12)
-graph.add_edge('C', 'D', 11)
-graph.add_edge('C', 'E', 3)
-graph.add_edge('C', 'G', 9)
-graph.add_edge('D', 'C', 11)
-graph.add_edge('D', 'E', 11)
-graph.add_edge('D', 'F', 10)
-graph.add_edge('E', 'G', 7)
-graph.add_edge('E', 'F', 6)
-graph.add_edge('G', 'A', 12)
-graph.add_edge('G', 'C', 9)
-graph.add_edge('G', 'E', 7)
-graph.add_edge('G', 'F', 9)
-
-algorithm = input("Ketikkan algorithm yang ingin digunakan ([1]TSP / [2]Dijkstra): ")
-
-if algorithm == "1":
-    start = input("Masukkan vertex awal: ")
-    end = input("Masukkan vertex tujuan: ")
-
-    shortest_path, shortest_distance, all_paths, execution_time = tsp(graph, start, end)
-
-    print("\nHasil TSP:")
-    print("-------------")
-    print("Shortest Path:")
-    print_path(shortest_path, shortest_distance)
-    print("\nAll Paths:")
-    print_all_paths(all_paths, start, end)
-    print("Waktu eksekusi:", execution_time, "seconds")
-
-elif algorithm == "2":
-    start = input("Masukkan vertex awal: ")
-    end = input("Masukkan vertex tujuan: ")
-
-    shortest_path, shortest_distance, distances, execution_time = dijkstra(graph, start, end)
-
-    print("\nHasil Dijkstra:")
-    print("-----------------")
-    print_path(shortest_path, shortest_distance)
-    print("\nJarak:")
-    for vertex, distance in distances.items():
-        print(f"Jarak dari {start} ke {vertex}: {distance}")
-    print("Waktu eksekusi:", execution_time, "seconds")
-
-else:
-    print("Pilihan tidak valid!.")
+if __name__ == '__main__':
+    main()
